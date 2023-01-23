b5c94603d8e1adb75c9978cd6b44c9e14f23a525
Romulo Rosa <romulo.frontend@gmail.com>
2023-01-22 23:18:46 -0300

    Etapa-02 Aula-02-02 Conditional If

---
+++ Etapa-02/Aula-02-02/js/app.js
@@ -0,0 +1,55 @@
+//Etapa-02 aula-02-02 CONDITIONAL IF
+
+/*
+IF
+- Estrutura condicional
+- Verifica SE uma determinada condição é verdadeira
+- Caso seja, o bloco de código é executado uma vez
+*/
+
+
+
+/*
+SINTAXE - EXPLICAÇÃO
+if (condição){
+  bloco de código
+}
+*/
+
+
+
+/*EXEMPLO PRÁTICO 01 - TESTE DE VARIAVEL*/
+
+// variavel para armazenar um determinado valor. 
+const idade = 18;
+
+// tipo de condicional (condição)
+// se a condição for verdadeira o bloco de código será executado
+if (idade >= 18){
+
+  //bloco de código
+  console.log("Igual ou Maior de 18 anos")
+}
+
+
+
+/*EXEMPLO PRÁTICO 02 - TESTE DE ARRAY*/
+
+// variavel para armazenar um determinado valor / array.
+const simpsons = ["Bart", "Home", "Marge", "Lisa", "Maggie"]
+
+// tipo de condicional (condição)
+// se a condição for verdadeira o bloco de código será executado
+if (simpsons.length > 3){
+
+  //bloco de código
+  console.log("Os Simpsons possuem mais de 3 personsagens principais")
+}
+
+/*
+FLUXO do código
+1) if | tipo de condicional. Vai vaerificar se a condição é true
+2) (condição) | Caso seja verdadeira pode executar o bloco de código
+3) { bloco de código } | Trecho de código a ser executado
+4) Caso haja alguma linha de código após bloco de código essa será executada
+*/
